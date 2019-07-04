2bba80de4e3e2580028ccc565b5c2ef8c5ea3e59
juancr5 <37746221+juancr5@users.noreply.github.com>
2019-07-04 16:42:04 -0500

    Update P1.c

--- Estructuras De Programación/P1.c
+++ Estructuras De Programación/P1.c
@@ -1,32 +1,49 @@
+/*
+P1. El propietario de Harry’s Car Sales paga a cada vendedor una comisión basada en sus ventas trimestrales. Los rangos de ventas y las correspondientes tasas son mostrades en la tabla que aparece a continuación:
+
+Ventas trimestrales ($)	Comisión
+0 - 20000	||  Multiplicar las ventas por 5%
+20001-50000	||  Multiplicar la ventas sobre 20000 por 7% y entonces agregue 1000 al resultado
+50001 o mas	||  Multiplicar la ventas sobre 50000 por 10% y entonces agregue 3100 al resultado
+
+Teniendo en cuenta lo anterior lleve a cabo las siguientes tareas:
+
+Comprenda el problema y realice casos de obteniendo las comisiones para los siguientes valores de ventas: 20000, 20001, 30000, 50000, 50001, 75000, y –3.
+Codifique el algoritmo en C de modo que permita el calculo de las comisiones. Lleve a cabo las pruebas con los casos de test elegidos en el item anterior.
+*/
+
 #include <stdio.h>
+float calcularComision(x);
 
 int main()
 {
     int x;
+    float comision;
     printf("Introduzca las ventas del trimestre: \n");
     scanf("%d", &x);
-    calcularComision(x); /* llamada a la funcion */
+    comision = calcularComision(x); /* llamada a la funcion */
+    printf("La comision del trimestre es % .4f \n", comision);
     return 0;
 }
 
-void calcularComision(int x)
+float calcularComision(int x)
 {
-    double y;
+    double comision;
     switch (x)
     {
     case 0 ... 20000:
-        y = 0.05 * x;
-        printf("La comision del trimestre es %4.4f \n", y);
+        comision = (float) 0.05 * x;
+        return comision;
         break;
 
     case 20001 ... 50000:
-        y = 1000 + 0.07 * x;
-        printf("La comision del trimestre es %4.4f \n", y);
+        comision = 1000 + 0.07 * x;
+        return comision;
         break;
 
     case 50001 ... 500000:
-        y = 3100 + 0.1 * x;
-        printf("La comision del trimestre es %5.4f\n", y);
+        comision = 3100 + 0.1 * x;
+        return comision;
         break;
 
     default:
