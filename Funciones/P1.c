d1d3d0da0ef57469a286bd63f426e1d6b5dd719c
juancr5 <37746221+juancr5@users.noreply.github.com>
2019-07-05 21:54:35 -0500

    Add files via upload

---
+++ Funciones/P1.c
@@ -0,0 +1,33 @@
+/*
+ P1. Escriba una función llamada múltiplo que tome dos enteros y determine si el segundo es múltiplo del primero.
+ La función deberá tomar dos argumentos enteros y devolver 1 si el segundo es un múltiplo del primero y 0 si no. Luego,
+ utilice esta función en un programa que acepte como entrada una serie de pares de enteros.
+ */
+
+#include <stdio.h>
+
+int main() {
+	int primero;
+	int segundo;
+	int multiplicidad;
+	printf("ingrese primer digito:");
+	scanf("%d", &primero);
+	printf("ingrese segundo digito:");
+	scanf("%d", &segundo);
+	multiplicidad = multiplo(primero, segundo);
+
+	if (multiplicidad == 1) {
+		printf("\n%d es multiplo de %d", segundo, primero);
+	} else {
+		printf("\n%d no es multiplo de %d", segundo, primero);
+	}
+	return 0;
+}
+
+int multiplo(int x, int y) {
+	if ((y % x) == 0) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
